ec5ab48a374a58af3cb176b09f5d3fc396171d72
Junchao Wang <junchaowang@u.boisestate.edu>
2021-12-17 20:35:50 -0700

    rotation feature

--- app.js
+++ app.js
@@ -2,24 +2,30 @@ document.addEventListener('DOMContentLoaded', () => {
 
     /* Global variable section */
     //board data 
-    const grid = document.getElementById('grid')
+    const grid = createGrid()
     const squares = Array.from(grid.querySelectorAll('div'))
 
+    let reachBottom = false
     let currentPosition = 4
     let width = 10
-    let adjustment = 10
     let timeId
     let lastRowIndex = buildLastRowIndex()
 
     // Tetromino data
     const oShape = [0, 1, width, width+1]
-    const zShape = [0, 1, width+1, width+2]
+    const zShape = [[0, 1, width+1, width+2],
+                    [2, width+1, width+2, width*2+1],
+                    [0, 1, width+1, width+2],
+                    [2, width+1, width+2, width*2+1]]
+
     const lShape = [0, width, width+1, width+2]
     const iShape = [0, width, width*2, width*3]
     const tShape = [width, 1, width+1, width+2]
     const iTetromino = [oShape, zShape, lShape, iShape, tShape]
     let randomShape = Math.floor(Math.random() * iTetromino.length)
-    let currentShape = iTetromino[randomShape]
+    let rotationIndex = 0
+    let currentShape = iTetromino[1][rotationIndex]
+
 
 
     // brick color data 
@@ -36,6 +42,18 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
     /* Data initialization section */
+    function createGrid() {
+        // the main grid
+        let grid = document.getElementById("grid")
+        for (let i = 0; i < 200; i++) {
+          let cell = document.createElement("div")
+          cell.classList.add('cell')
+          grid.appendChild(cell)
+        }
+        return grid
+      }
+    
+
     function buildLastRowIndex() {
         let lastRow = []
         for (let i=190; i<200; i++) {
@@ -47,17 +65,27 @@ document.addEventListener('DOMContentLoaded', () => {
     /* Navigation functions section */
     function moveDown() {
         undrawOnGrid()
-        currentPosition += adjustment
+        currentPosition = currentPosition += width
         drawOnGrid()
         freeze()
     }
 
     function moveLeft() {
         undrawOnGrid()
-        if(currentPosition % width != 0) {
+        let isOnLeftEdge = currentShape.some(index => (index + currentPosition) % width === 0)
+        if(!isOnLeftEdge) {
             currentPosition -= 1
         }
         drawOnGrid()
+
+    }
+
+    function drop() {
+        while(!reachBottom) {
+            moveDown()
+        }          
+
+        reachBottom = false
     }
 
     function moveRight() {
@@ -69,13 +97,15 @@ document.addEventListener('DOMContentLoaded', () => {
         drawOnGrid()
     }
 
-    let drop =() => {
-        let i = 0
-        while(i < 15){
-            moveDown()
-            i++
+    function rotate() {
+        undrawOnGrid()
+        rotationIndex ++
+        if(rotationIndex > 3) {
+            rotationIndex = 0
         }
-        currentPosition = 3 
+        currentShape = iTetromino[1][rotationIndex]
+        drawOnGrid()
+
     }
 
     function freeze() {
@@ -102,69 +132,102 @@ document.addEventListener('DOMContentLoaded', () => {
         if (isBottom || isSettled) {
             currentShape.forEach(index => {
                 squares[index + currentPosition].style.backgroundImage = currentColor
+                squares[index + currentPosition].style.backgroundSize = '20px'
                 squares[index + currentPosition].classList.add("block")
             })
             currentPosition = 4
             randomColor = Math.floor(Math.random() * brickColor.length)
             currentColor = brickColor[randomColor]
             randomShape = Math.floor(Math.random() * iTetromino.length)
-            currentShape = iTetromino[randomShape]
+            currentShape = iTetromino[1][rotationIndex]
+          
+            reachBottom = true
+            continueGame = false
             drawOnGrid()
-            
-            ids.forEach(id => {
-                clearInterval(id)
-            })
         }
     }
 
     /* action fuctions section*/
     let keys = []
-    let ids = []
-    let downSpeed = 50
-    
-    function keydown(e) {
-        console.log(keys)
-        keys[e.keyCode] = (e.type == "keydown")
-        if(keys && keys[37]) {
-            ids.push(setInterval(moveLeft, 50))
+
+    function move() {
+        let isOnLeftEdge = currentShape.some(index => (index + currentPosition) % width === 0)
+        if(keys && keys[37] && !isOnLeftEdge) {
+            moveLeft()
         }
-        if(keys && keys[39]) {
-            ids.push(setInterval(moveRight, 50))
+        let isOnRightEdge = currentShape.some(index => (index + currentPosition) % width === width - 1)
+        if(keys && keys[39] && ! isOnRightEdge) {
+            moveRight()
         }
         if (keys && keys[40]) {
-            ids.push(setInterval(moveDown, downSpeed))
-            if(downSpeed < 100) {
-                downSpeed = downSpeed + 100
-            }
-        }  
-        if (e.keyCode === 32) {
-            ids.push(setInterval(moveDown, 0.1))
-        } 
+            moveDown()
+        }
+
+    }
+    
+    let dropDisable = false
+    let rotateDisable = false
+    function keydown(e) {
+        keys[e.keyCode] = (e.type == "keydown")
+
+        if (e.keyCode === 32 && !dropDisable) {
+            drop()
+            dropDisable = true
+        }
+        if(e.keyCode === 38 && !rotateDisable) {
+            rotate()
+            rotateDisable = true
+        }
+
     }
 
     function keyUp(e) {
         keys[e.keyCode] = (e.type == "keydown")
-        ids.forEach(id => {
-            clearInterval(id)
-            downSpeed = 50
-        })
+        if (e.keyCode === 32) {
+            dropDisable = false
+        }
+        if (e.keyCode === 38) {
+            rotateDisable = false
+        }
     }
 
     function drawOnGrid() {
         currentShape.forEach(index => {
+            squares[index + currentPosition].style.backgroundSize = '20px'
             squares[index + currentPosition].style.backgroundImage = currentColor
         })
     }
 
     function undrawOnGrid() {
         currentShape.forEach(index => {
-            squares[index + currentPosition].style.backgroundImage = "none"
+            squares[index + currentPosition].style.backgroundImage = 'none'
         })
     }
 
-    drawOnGrid()
-    document.addEventListener('keydown', keydown);
+    function sound(src) {
+        this.sound = document.createElement("audio")
+        this.sound.src = src;
+        this.sound.setAttribute("preload", "auto")
+        this.sound.setAttribute("controls", "none")
+        this.sound.style.display = "none"
+        this.sound.loop = true
+        document.body.appendChild(this.sound)
+        this.play = function(){
+            this.sound.play()
+        }
+        this.stop = function(){
+            this.sound.pause()
+        }    
+    }
+
+
+    
+    mySound = new sound("Tetris.mp3");
+    //mySound.play();
+   // drawOnGrid()
+    document.addEventListener('keydown', keydown)
     document.addEventListener('keyup', keyUp)
+    setInterval(move, 60); 
     timeId = setInterval(moveDown, 1000)
 
 })
